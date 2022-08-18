7285d45a3ac1f9e28f50ebca3e7bb07f88ab4ad4
Gustavo <gustalmeida02@gmail.com>
2022-08-18 19:37:08 -0300

    Primeiro teste concluído com êxito

--- Empregado.java
+++ Empregado.java
@@ -1,47 +1,76 @@
-public class Empregado {
-    private String nome;
+public class Empregado{
     private int tipo;
     private double salario;
     private double comissao;
     private double bonus;
-
-
-    public void setTipo(int tipo){
-        this.tipo = tipo;
-    }
-
-    public int getTipo(){
-        return tipo;
-    }
-
-    public double calculaSalario(){
-        if (tipo == 1)
-            return salario;
-        else if (tipo == 2)
-            return salario + salario * comissao;
-        else if (tipo == 3)
-            return salario + bonus;
-        else
-            return 0;            
-    }
-
-
-
+    private String nome;
     private int idade;
-
+  
+    public double getSalario() {
+      return salario;
+    }
+  
+    public double getComissao() {
+      return comissao;
+    }
+    public double getBonus() {
+      return bonus;
+    }
+  
+    public void setSalario(double salario) {
+      this.salario = salario;
+    }
+  
+    public void setComissao(double comissao) {
+      this.comissao = comissao;
+    }
+  
+    public void setBonus(double bonus) {
+      this.bonus = bonus;
+    }
+  
+    public void setTipo(int tipo){
+      this.tipo = tipo;
+    }
+  
+    public int getTipo(){
+      return tipo;
+    }
+  
+    public double calculaSalario(){
+      if (tipo == 1)
+        return salario;
+      else if (tipo == 2)
+        return salario + salario * comissao;
+      else if (tipo == 3)
+        return salario + bonus;
+      else
+        return 0;
+    }
+  
     public String getNome() {
+      return nome;
+    }
+  
+    public int getIdade() {
+      return idade;
+    }
+  
+    public void setNome(String nome) {
         return nome;
     }
-
+  
     public int getIdade() {
-        return idade;
+      return idade;
     }
-
+  
     public void setNome(String nome) {
-        this.nome = nome;
+      this.nome = nome;
     }
-
+  
     public void setIdade(int idade) {
-        this.idade = idade;
+      this.idade = idade;
     }
-}
+  
+  }
+  
--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -4,6 +4,22 @@ public class TesteEmpregado {
         Empregado e1 = new Empregado();
         Empregado e2 = new Empregado();
         Empregado e3 = new Empregado();
+
+        e1.setTipo(tipo: 1);
+        e2.setTipo(tipo: 2);
+        e3.setTipo(tipo: 3);
+
+        e1.setSalario(salario: 2000);
+
+        e2.setSalario(salario: 1700);
+        e2.setComissao(comissao: 0.2);
+
+        e3.setSalario(1500);
+        e3.setBonus(bonus: 400);
+
+        System.out.println(e1.calculaSalario());
+        System.out.println(e2.calculaSalario());
+        System.out.println(e3.calculaSalario());
     
     }
     
